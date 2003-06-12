a4e59508bad5ab81e0e25aa6d49d49e110be9ba0
Kevin M. Rosenberg <kevin@rosenberg.net>
2003-06-12 14:18:12 +0000

    r5111: *** empty log message ***

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -e
 
-dup cl-base64 -Uftp.b9.com -D/home/ftp/cl-base64 -C"/home/kevin/bin/remove-old-versions cl-base64" -su $*
-
+dup cl-base64 -Uftp.b9.com -D/home/ftp/cl-base64 -C"/home/kevin/bin/remove-old-versions cl-base64 latest" -su $*
