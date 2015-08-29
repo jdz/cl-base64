fc62a5342445d4ec1dd44e95f7dc513473a8c89a
Kevin M. Rosenberg <kevin@rosenberg.net>
2015-08-29 00:15:51 -0600

    Update domain name to kpe.io

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-dup cl-base64 -Ufiles.b9.com -D/home/ftp/cl-base64 -su \
+dup cl-base64 -Ufiles.kpe.io -D/home/ftp/cl-base64 -su \
     -C"(umask 022; cd /srv/www/html/cl-base64; make install; find . -type d |xargs chmod go+rx; find . -type f | xargs chmod go+r)" $*
